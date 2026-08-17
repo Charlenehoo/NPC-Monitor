e14b54fc0249fdaf0036d2029aa6f2a566024acd
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-17 11:45:44 +0800

    10

--- lua/autorun/server/npc_monitor.lua
+++ lua/autorun/server/npc_monitor.lua
@@ -8,14 +8,17 @@ local function AddHook(eventName, func)
 end
 
 local activeNPCS = setmetatable({}, { __mode = "k" })
-local lastSchedules = setmetatable({}, { __mode = "k" })  -- npc -> schedule
-local lastStates = setmetatable({}, { __mode = "k" })     -- npc -> state
-local lastConditions = setmetatable({}, { __mode = "k" }) -- npc -> { condition -> has}
+local lastSchedules = setmetatable({}, { __mode = "k" }) -- { npc -> schedule }
+local lastStates = setmetatable({}, { __mode = "k" })    -- { npc -> state }
+
+local allLastConditions = {}                             -- { condition -> { npc -> has } }
+for name, id in pairs(COND) do
+    allLastConditions[name] = setmetatable({}, { __mode = "k" })
+end
 
 AddHook("InitPostEntity", function()
     for _, ent in ipairs(ents.GetAll()) do
         if not IsValid(ent) or not ent:IsNPC() then continue end
-
         activeNPCS[ent] = true
     end
 end)
@@ -42,8 +45,20 @@ AddHook("Tick", function()
             hook.Run("OnStateChange", npc, lastState, currentState)
             lastStates[npc] = currentState
         end
+    end
 
-        -- local
+    for name, id in pairs(COND) do
+        local lastConditions = allLastConditions[name] -- { npc -> has }
+        for npc in pairs(activeNPCS) do
+            if not IsValid(npc) then continue end
+
+            local lastCondition = lastConditions[npc]
+            local currentCondition = npc:HasCondition(id)
+            if lastCondition ~= currentCondition then
+                hook.Run("OnCondition", npc, name, id, lastCondition, currentCondition)
+                lastConditions[npc] = currentCondition
+            end
+        end
     end
 end)
 
@@ -68,5 +83,14 @@ AddHook("OnStateChange", function(npc, lastState, currentState)
         npc, "StateChange: ",
         getStateName(lastState), " -> ",
         getStateName(currentState)
+    )
+end)
+
+AddHook("OnConditionChange", function(npc, conditionName, conditionID, lastValue, currentValue)
+    if not shouldLog(npc) then return end
+    log.trace(
+        npc, "ConditionChange: ",
+        conditionName, " (ID:", conditionID, "): ",
+        tostring(lastValue), " -> ", tostring(currentValue)
     )
 end)
