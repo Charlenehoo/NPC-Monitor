c531f7184a3fef2b3eb8c2cfb5e6caba78cab426
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-19 09:17:24 +0800

    5

--- lua/autorun/server/enhance_emit_sound_vox.lua
+++ lua/autorun/server/enhance_emit_sound_vox.lua
@@ -1,8 +1,9 @@
-local log = include("npc_monitor/log.lua")
-local addHook = include("npc_monitor/add_hook.lua")
 local CONSTANTS = include("npc_monitor/constants.lua")
+local log = include("npc_monitor/log.lua")
+local helpers = include("npc_monitor/helpers.lua")
+local addUniqueHook = helpers.addUniqueHook
 
-addHook("InitPostEntity", function()
+addUniqueHook("InitPostEntity", function()
     local meta = FindMetaTable("Player")
     if not meta or not meta.EmitSoundVOX then return end
 
@@ -12,4 +13,4 @@ addHook("InitPostEntity", function()
         originalEmitSoundVOX(self, sndid)
         sound.EmitHint(SOUND_PLAYER, self:GetPos(), CONSTANTS.SOUND_HINT_RADIUS, CONSTANTS.SOUND_HINT_LAST, self)
     end
-end, "EnhanceEmitSoundVOX")
+end)
