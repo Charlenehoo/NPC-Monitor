fd029b49c76008b50d0f1030bba82242ecc4b9ec
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-21 08:53:09 +0800

    增加事件名常量

---
+++ lua/npc_monitor/core/events.lua
@@ -0,0 +1,10 @@
+-- core/events.lua
+
+local M             = {}
+
+M.TranslateSchedule = "NPCMonitor.TranslateSchedule"
+M.OnStateChange     = "NPCMonitor.OnStateChange"
+M.OnEnemyChange     = "NPCMonitor.OnEnemyChange"
+M.OnCondition       = "NPCMonitor.OnCondition"
+
+return M
