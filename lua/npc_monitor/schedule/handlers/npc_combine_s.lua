20f0bbb6303253ee3a103dd8bbff5fc753bd4cbf
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-21 13:16:55 +0800

    增加进入条件

--- lua/npc_monitor/schedule/handlers/npc_combine_s.lua
+++ lua/npc_monitor/schedule/handlers/npc_combine_s.lua
@@ -4,50 +4,60 @@ local log       = include("npc_monitor/logging/log.lua")
 local helpers   = include("npc_monitor/helpers.lua")
 
 local function idleHandler(npc, lastSchedule, currentSchedule)
-    local dummyOfChoice
+    if lastSchedule == SCHED_ALERT_STAND then
+        local dummyOfChoice
 
-    NPCMonitor.ForEachActiveDummy(function(dummy)
-        if IsValid(dummy) then
-            dummyOfChoice = dummy
-            return
+        NPCMonitor.ForEachActiveDummy(function(dummy)
+            if IsValid(dummy) then
+                dummyOfChoice = dummy
+                return true
+            end
+        end)
+
+        if dummyOfChoice then
+            npc:SetTarget(dummyOfChoice)
+            return SCHED_TARGET_CHASE
         end
-    end)
-    if dummyOfChoice then
-        npc:SetTarget(dummyOfChoice)
-        return SCHED_TARGET_CHASE
     end
 
     return nil
 end
 
 local function alertHandler(npc, lastSchedule, currentSchedule)
-    local dummyOfChoice
+    if lastSchedule == SCHED_ALERT_STAND then
+        local dummyOfChoice
 
-    NPCMonitor.ForEachActiveDummy(function(dummy)
-        if IsValid(dummy) then
-            dummyOfChoice = dummy
-            return true
+        NPCMonitor.ForEachActiveDummy(function(dummy)
+            if IsValid(dummy) then
+                dummyOfChoice = dummy
+                return true
+            end
+        end)
+
+        if dummyOfChoice then
+            npc:SetTarget(dummyOfChoice)
+            return SCHED_TARGET_CHASE
         end
-    end)
-    if dummyOfChoice then
-        npc:SetTarget(dummyOfChoice)
-        return SCHED_TARGET_CHASE
     end
 
     return nil
 end
 
 local function combatHandler(npc, lastSchedule, currentSchedule)
-    if (lastSchedule == Enum.COMBINE_SCHEDULE_ENUM.SCHED_COMBINE_RANGE_ATTACK1 or
-            lastSchedule == SCHED_RANGE_ATTACK1) and
-        npc:HasCondition(COND.ENEMY_OCCLUDED) then
-        return SCHED_SHOOT_ENEMY_COVER
-    end
+    if currentSchedule ~= Enum.COMBINE_SCHEDULE_ENUM.SCHED_COMBINE_HIDE_AND_RELOAD and
+        currentSchedule ~= SCHED_RELOAD then
+        if (lastSchedule == Enum.COMBINE_SCHEDULE_ENUM.SCHED_COMBINE_RANGE_ATTACK1 or
+                lastSchedule == Enum.COMBINE_SCHEDULE_ENUM.SCHED_COMBINE_ESTABLISH_LINE_OF_FIRE or
+                lastSchedule == SCHED_RANGE_ATTACK1)
+            and
+            npc:HasCondition(COND.ENEMY_OCCLUDED) then
+            return SCHED_SHOOT_ENEMY_COVER
+        end
 
-    if lastSchedule == SCHED_SHOOT_ENEMY_COVER and
-        npc:HasCondition(COND.ENEMY_OCCLUDED) and
-        not npc:HasCondition(COND.LOST_ENEMY) then
-        return SCHED_SHOOT_ENEMY_COVER
+        if lastSchedule == SCHED_SHOOT_ENEMY_COVER and
+            not npc:HasCondition(COND.LOST_ENEMY) then
+            return SCHED_SHOOT_ENEMY_COVER
+        end
     end
 
     return nil
