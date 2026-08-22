eea425ff6afb0c1b516d44c196f628158fa193ef
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-22 13:28:14 +0800

    骨骼表

---
+++ lua/npc_monitor/config/bone.lua
@@ -0,0 +1,77 @@
+-- 按照优先级排列的所有候选骨骼（根据 ValveBiped 标准层级表）
+local BONE_FALLBACK_ORDER = {
+    -- 头部和颈部（最接近眼睛）
+    "ValveBiped.Bip01_Head1",
+    "ValveBiped.Bip01_Neck1",
+
+    -- 躯干（从高到低）
+    "ValveBiped.Bip01_Spine4", -- 可选，若不存在则跳过
+    "ValveBiped.Bip01_Spine2",
+    "ValveBiped.Bip01_Spine1",
+    "ValveBiped.Bip01_Spine",
+    "ValveBiped.Bip01_Pelvis",
+
+    -- 左腿
+    "ValveBiped.Bip01_L_Thigh",
+    "ValveBiped.Bip01_L_Calf",
+    "ValveBiped.Bip01_L_Foot",
+    "ValveBiped.Bip01_L_Toe0",
+
+    -- 右腿
+    "ValveBiped.Bip01_R_Thigh",
+    "ValveBiped.Bip01_R_Calf",
+    "ValveBiped.Bip01_R_Foot",
+    "ValveBiped.Bip01_R_Toe0",
+
+    -- 左臂
+    "ValveBiped.Bip01_L_Clavicle",
+    "ValveBiped.Bip01_L_UpperArm",
+    "ValveBiped.Bip01_L_Forearm",
+    "ValveBiped.Bip01_L_Hand",
+    -- 左手手指（拇指）
+    "ValveBiped.Bip01_L_Finger0",
+    "ValveBiped.Bip01_L_Finger01",
+    "ValveBiped.Bip01_L_Finger02",
+    -- 左手食指
+    "ValveBiped.Bip01_L_Finger1",
+    "ValveBiped.Bip01_L_Finger11",
+    "ValveBiped.Bip01_L_Finger12",
+    -- 左手中指
+    "ValveBiped.Bip01_L_Finger2",
+    "ValveBiped.Bip01_L_Finger21",
+    "ValveBiped.Bip01_L_Finger22",
+    -- 左手无名指
+    "ValveBiped.Bip01_L_Finger3",
+    "ValveBiped.Bip01_L_Finger31",
+    "ValveBiped.Bip01_L_Finger32",
+    -- 左手小指
+    "ValveBiped.Bip01_L_Finger4",
+    "ValveBiped.Bip01_L_Finger41",
+    "ValveBiped.Bip01_L_Finger42",
+
+    -- 右臂
+    "ValveBiped.Bip01_R_Clavicle",
+    "ValveBiped.Bip01_R_UpperArm",
+    "ValveBiped.Bip01_R_Forearm",
+    "ValveBiped.Bip01_R_Hand",
+    -- 右手手指（拇指）
+    "ValveBiped.Bip01_R_Finger0",
+    "ValveBiped.Bip01_R_Finger01",
+    "ValveBiped.Bip01_R_Finger02",
+    -- 右手食指
+    "ValveBiped.Bip01_R_Finger1",
+    "ValveBiped.Bip01_R_Finger11",
+    "ValveBiped.Bip01_R_Finger12",
+    -- 右手中指
+    "ValveBiped.Bip01_R_Finger2",
+    "ValveBiped.Bip01_R_Finger21",
+    "ValveBiped.Bip01_R_Finger22",
+    -- 右手无名指
+    "ValveBiped.Bip01_R_Finger3",
+    "ValveBiped.Bip01_R_Finger31",
+    "ValveBiped.Bip01_R_Finger32",
+    -- 右手小指
+    "ValveBiped.Bip01_R_Finger4",
+    "ValveBiped.Bip01_R_Finger41",
+    "ValveBiped.Bip01_R_Finger42",
+}
