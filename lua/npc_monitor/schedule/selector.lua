1d4a0d844d733ea6a2c5eff6e4f7f07ef430cb2d
CharleneHoo <CharleneHoo@hotmail.com>
2026-08-22 11:54:29 +0800

    简化

--- lua/npc_monitor/schedule/selector.lua
+++ lua/npc_monitor/schedule/selector.lua
@@ -12,10 +12,21 @@ local function selectSchedule(npc, lastSchedule, currentSchedule)
     local enemy = npc:GetEnemy()
     if IsValid(enemy) and enemy:GetClass() == CONSTANTS.RAGADOLL_DUMMY_CLASS then return nil end
 
-    local handler = NPCMonitor.ScheduleHandlers[npc:GetClass()]
-    if handler then
-        return handler(npc, lastSchedule, currentSchedule)
+    if currentSchedule == SCHED_IDLE_STAND or
+        currentSchedule == SCHED_ALERT_STAND then
+        local candidates = {}
+        NPCMonitor.ForEachActiveDummy(function(dummy)
+            if IsValid(dummy) and dummy:IsPotentialExecutioner(npc) then
+                table.insert(candidates, dummy)
+            end
+        end)
+        if #candidates > 0 then
+            local dummyOfChoice = candidates[math.random(#candidates)]
+            npc:SetTarget(dummyOfChoice)
+            return SCHED_TARGET_CHASE
+        end
     end
+
     return nil
 end
 
